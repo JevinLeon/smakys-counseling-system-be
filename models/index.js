97d97b48ebef23e56993978aa78cc84609b2537c
JevinLeon <jevinleon@gmail.com>
2024-05-26 23:36:15 +0700

    deploy

--- models/index.js
+++ models/index.js
@@ -8,18 +8,22 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/sequelize.js")[env];
 const db = {};
+const pg = require("pg");
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
+const sequelize = new Sequelize("postgres://admin:admin@localhost:5432/mydb", {
+  dialectModule: pg,
+});
 
 fs.readdirSync(__dirname)
   .filter((file) => {
