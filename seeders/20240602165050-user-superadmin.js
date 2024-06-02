8e0bf6fb34f685b233e4827ac880ef5b03b9f98b
JevinLeon <jevinleon@gmail.com>
2024-06-02 23:53:22 +0700

    deploy

---
+++ seeders/20240602165050-user-superadmin.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        name: "Superadmin",
+        username: "superadmin",
+        role: "superadmin",
+        password: "123123",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
